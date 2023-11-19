6eb1c8f0de2624a689ebfa7d6c9005d51fde5323
unknown <avesta9312@gmail.com>
2023-11-19 16:42:01 +0330

    don

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,40 +1,52 @@
 import { stringify } from 'postcss';
-import { React, useEffect, useState } from 'react'
+import { React, useState } from 'react'
 
 const App = () => {
 
   const [data, setData] = useState([])
   const [input, setInput] = useState('')
-  var word;
+  const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(() => {
-    fetch(`https://api.datamuse.com/words?ml=${input}`)
-      .then(response => response.json())
-      .then(json => setData(json.slice(0, 10)))
-      .catch(error => console.error(error));
-  }, [input]);
-
+ function fetchDate() {
+  setIsLoading(true)
+  fetch(`https://api.datamuse.com/words?ml=${input}`)
+    .then(response => response.json())
+    .then(json => {
+      setData(json.slice(0, 10))
+      setIsLoading(false)
+    })
+    .catch(error => console.error(error));
+ }
 
   function handleClick() {
-    setInput(word)
+    fetchDate()
   }
 
   function handleChange(e) {
-    word = e.target.value
+    var word = e.target.value
+    setInput(word)
   }
+  
+  function handleListItem(itemWord) {
+    setInput(itemWord)
+  }
+  
 
 
   return (
     <div className='bg-[#202020] w-screen h-screen flex justify-center items-center gap-4 flex-col'>
       <div className='flex gap-2'>
-        <input type="text" onChange={handleChange} className='border-2 border-black rounded-lg p-2' />
+        <input type="text" value={input} onChange={handleChange} className='border-2 border-black rounded-lg p-2'></input>
         <button onClick={handleClick} className='text-black border-2 p-2 bg-gray-400 rounded-md'>submit</button>
       </div>
       <ul>
-        {data.map((item, index) => (
+        {isLoading 
+        ? <div className='text-white'>loading...</div> 
+        : data.map((item, index) => (
           <li 
           key={index}
           className='text-white cursor-pointer uppercase font-bold hover:shadow-inner hover:shadow-black rounded-lg p-1'
+          onClick={() => {handleListItem(item.word)}}
           >{item.word}</li>
         ))}
       </ul>
